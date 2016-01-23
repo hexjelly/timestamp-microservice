97a08730c18418269c4c68443bf1f6c4a55880d6
Roger Andersen <hexjelly@hexjelly.com>
2016-01-23 13:07:50 +0100

    Update tests

--- test/index.js
+++ test/index.js
@@ -1,27 +1,28 @@
 var request = require('supertest');
+var server;
 
 describe('Routes', function () {
-    var server;
-    beforeEach(function () {
-      delete require.cache[require.resolve('../server')];
-      server = require('../server').listen(3000);
-    });
-    afterEach(function (done) {
-      server.close(done);
-    });
-    it('/ responds with html', function (done) {
-      request(server)
-        .get('/')
-        .set('Accept', 'text/html')
-        .expect('Content-Type', /html/)
-        .expect(200, done);
-    });
+  before(function () {
+    server = require('../server').listen(3000);
+  });
 
-    it('/* responds with json', function (done) {
-      request(server)
-        .get('/123456')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
-    });
+  after(function () {
+    server.close();
+  });
+
+  it('/ responds with html', function (done) {
+    request(server)
+      .get('/')
+      .set('Accept', 'text/html')
+      .expect('Content-Type', /html/)
+      .expect(200, done);
+  });
+
+  it('/* responds with json', function (done) {
+    request(server)
+      .get('/123456')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
 });
