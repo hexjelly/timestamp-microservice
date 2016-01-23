ce83e01d1194a6f6449e9cc3e5faf198b939a7ed
Roger Andersen <hexjelly@hexjelly.com>
2016-01-23 02:08:39 +0100

    Put logic in own function

--- index.js
+++ index.js
@@ -2,18 +2,29 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+function getJSONTimeResult (time) {
+  var time = new Date(time);
+  var unixTime = time.valueOf();
+  var naturalTime = time.toLocaleString('en-us', { month: "long" }) + ' ' + time.getDate() + ', ' + time.getFullYear();
+  var result = {
+    'unix': null,
+    'natural': null
+  };
+
+  if (unixTime) {
+    result.unix = unixTime;
+    result.natural = naturalTime;
+  }
+  return result;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/html/index.html'));
 });
 
 app.get('/:time', function (req, res) {
-  var result = {};
-  var time = new Date(req.params.time);
-  if (time.valueOf()) {
-    res.json({ 'unix': time.valueOf(), 'natural': time.toLocaleString('en-us', { month: "long" }) + ' ' + time.getDate() + ', ' + time.getFullYear() });
-  } else {
-    res.json({ 'unix': null, 'natural': null });
-  }
+  res.json(getJSONTimeResult(req.params.time));
 });
 
 app.listen(3000, function () {
